9df36f5bbff0832e8e8f422ef305a809adc5768c
Mikhail Sholkov <ecoaw1@gmail.com>
2024-09-10 16:25:01 +0300

    [feature] moving average implementation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
-#include <iostream>
-
 #include "data_generator.hpp"
 #include "moving_average.hpp"
 
 int main()
 {
-    auto data = generate_data<float>(0.f, 1.f, 100);
-    auto res = moving_average<float>(data, 4);
-
     return 0;
 }
--- src/moving_average.hpp
+++ src/moving_average.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <numeric>
 #include <type_traits>
 #include <cassert>
 
@@ -11,9 +12,25 @@ template<typename T>
 std::vector<T> moving_average(const std::vector<T>& data, size_t window_sz)
 {
     static_assert(std::is_floating_point<T>::value, "[ERROR] Expected floating point type.");
-    assert(data.size() >= window_sz);
+
+    // По условию задачи размер окна начинается от 4.
+    assert(window_sz >= 4);
+
+    assert(data.size() > window_sz);
 
     std::vector<T> result{};
+    result.reserve(data.size());
+
+    // Вычисление начального значения.
+    T sum = std::accumulate(data.begin(), data.begin()+window_sz, T{});
+    sum /= window_sz;
+    result.push_back(sum);
+
+    // В силу линейности среднего:
+    for (int i = 1; i <= data.size()-window_sz; ++i) {
+        T next_mean = result.back() + (data[i+window_sz-1] - data[i-1])/window_sz;
+        result.push_back(next_mean);
+    }
 
     return result;
 }
